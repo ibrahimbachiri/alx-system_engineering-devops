520ec9bcd19ca0d3061228b8fcc88e3a82327a8f
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-10-31 03:34:40 -0700

    m

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -4,14 +4,10 @@
 if ARGV.empty?
   puts ""
 else
-  # Iterate through the command-line arguments
   ARGV.each do |arg|
-    # Match the regular expression /School/ in the input argument
     if arg.match(/School/)
-      # Print the matched string followed by the end of line character
       puts arg
     else
-      # Print an empty line if no match is found
       puts ""
     end
   end

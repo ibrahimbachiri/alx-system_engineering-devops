a66f36cf64c48e341c29522dc86dd6c3a4165952
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-10-31 03:41:12 -0700

    m

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,13 +1,3 @@
 #!/usr/bin/env ruby
-
-# Iterate through the command-line arguments
-ARGV.each do |arg|
-  # Match the regular expression /School/ in the input argument
-  if matched = arg.match(/School/)
-    # Print the matched string followed by the end of line character
-    puts matched[0]
-  else
-    # Print an empty line if no match is found
-    puts ""
-  end
-end
+# A regular expression that is simply matching School
+puts ARGV[0].scan(/School/).join
